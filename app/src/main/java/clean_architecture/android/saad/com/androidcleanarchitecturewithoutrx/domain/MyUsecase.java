a9b2d38e642c5ab7117b4238fa8ebd5776cda530
ahmedIbrahimSaad <36634294+ahmedIbrahimSaad@users.noreply.github.com>
2018-05-08 16:30:34 +0200

    do some work in presentation

--- app/src/main/java/clean_architecture/android/saad/com/androidcleanarchitecturewithoutrx/domain/MyUsecase.java
+++ app/src/main/java/clean_architecture/android/saad/com/androidcleanarchitecturewithoutrx/domain/MyUsecase.java
@@ -1,4 +1,17 @@
 package clean_architecture.android.saad.com.androidcleanarchitecturewithoutrx.domain;
 
-public class MyUsecase {
+import clean_architecture.android.saad.com.androidcleanarchitecturewithoutrx.base.domain.UseCase;
+
+public class MyUsecase extends UseCase<MyUsecase.RequestValues,MyUsecase.ResponseValue> {
+    @Override
+    protected void executeUseCase(RequestValues requestValues) {
+
+    }
+
+    class RequestValues implements UseCase.RequestValues{
+
+    }
+    class ResponseValue implements UseCase.ResponseValue{
+
+    }
 }
